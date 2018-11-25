b486bf092f508d194a5762fc44e931100932a7b0
Dmytro Gnatyshyn <di1202@ukr.net>
2018-11-25 17:38:44 +0200

    clean master

--- my-app/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import OlxApp from './components/OlxApp'
-
-
-ReactDOM.render(
-    <OlxApp/>,
-    document.getElementById('root')
-);
